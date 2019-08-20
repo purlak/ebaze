5a9a1b6b850f2d13d93685d2ddeeb3da8167fc8f
Purnima Lakshminarayanan <purnima@8thlight.com>
2019-08-20 12:35:57 -0400

    add welcome message, login and signup links

--- test/ebaze_web/controllers/page_controller_test.exs
+++ test/ebaze_web/controllers/page_controller_test.exs
@@ -3,6 +3,7 @@ defmodule EbazeWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to Ebaze!"
+    assert html_response(conn, 200) =~ "New auction items listed everyday. Sell or Bid now!"  
   end
 end
