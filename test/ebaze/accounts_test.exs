c95fa4d38bdf50b7153efee04fd8a39e25850684
Purnima Lakshminarayanan <purnima@8thlight.com>
2019-08-29 08:50:22 -0400

    [FIX] add describe blocks in tests

--- test/ebaze/accounts_test.exs
+++ test/ebaze/accounts_test.exs
@@ -3,22 +3,22 @@ defmodule Ebaze.AccountsTest do
 
   alias Ebaze.Accounts
 
-  describe "users" do
-    alias Ebaze.Accounts.User
+  alias Ebaze.Accounts.User
 
-    @valid_attrs %{password: "some password", username: "some username"}
-    @update_attrs %{password: "some updated password", username: "some updated username"}
-    @invalid_attrs %{password: nil, username: nil}
+  @valid_attrs %{password: "some password", username: "some username"}
+  @update_attrs %{password: "some updated password", username: "some updated username"}
+  @invalid_attrs %{password: nil, username: nil}
 
-    def user_fixture(attrs \\ %{}) do
-      {:ok, user} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Accounts.create_user()
+  def user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Enum.into(@valid_attrs)
+      |> Accounts.create_user()
 
-      user
-    end
+    user
+  end
 
+  describe "get users" do
     test "list_users/0 returns all users" do
       user = user_fixture()
       assert Accounts.list_users() == [user]
@@ -28,7 +28,9 @@ defmodule Ebaze.AccountsTest do
       user = user_fixture()
       assert Accounts.get_user!(user.id) == user
     end
+  end
 
+  describe "create user" do
     test "create_user/1 with valid data creates a user" do
       assert {:ok, %User{} = user} = Accounts.create_user(@valid_attrs)
       assert {:ok, user} == Bcrypt.check_pass(user, "some password", hash_key: :password)
@@ -38,7 +40,9 @@ defmodule Ebaze.AccountsTest do
     test "create_user/1 with invalid data returns error changeset" do
       assert {:error, %Ecto.Changeset{}} = Accounts.create_user(@invalid_attrs)
     end
+  end
 
+  describe "modify users" do
     test "update_user/2 with valid data updates the user" do
       user = user_fixture()
       assert {:ok, %User{} = user} = Accounts.update_user(user, @update_attrs)
@@ -57,12 +61,16 @@ defmodule Ebaze.AccountsTest do
       assert {:ok, %User{}} = Accounts.delete_user(user)
       assert_raise Ecto.NoResultsError, fn -> Accounts.get_user!(user.id) end
     end
+  end
 
+  describe "return user struct" do
     test "change_user/1 returns a user changeset" do
       user = user_fixture()
       assert %Ecto.Changeset{} = Accounts.change_user(user)
     end
+  end
 
+  describe "user validations" do
     test "creating two users with the same username" do
       changeset =
         %User{}
--- test/ebaze_web/controllers/page_controller_test.exs
+++ test/ebaze_web/controllers/page_controller_test.exs
@@ -1,9 +1,11 @@
 defmodule EbazeWeb.PageControllerTest do
   use EbazeWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Ebaze!"
-    assert html_response(conn, 200) =~ "New auction items listed everyday. Sell or Bid now!"
+  describe "get landing page " do
+    test "GET /", %{conn: conn} do
+      conn = get(conn, "/")
+      assert html_response(conn, 200) =~ "Welcome to Ebaze!"
+      assert html_response(conn, 200) =~ "New auction items listed everyday. Sell or Bid now!"
+    end
   end
 end
