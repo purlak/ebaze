597552ee3954b319028c15c8aab0cf436338a43c
Purnima Lakshminarayanan <purnima@8thlight.com>
2019-08-30 15:16:18 -0400

    [FIX] test description

--- test/ebaze_web/controllers/session_controller_test.exs
+++ test/ebaze_web/controllers/session_controller_test.exs
@@ -8,12 +8,12 @@ defmodule EbazeWeb.SessionControllerTest do
   }
 
   describe "new session" do
-    test "renders sign-in form", %{conn: conn} do
+    test "GET /sign-in renders sign-in form", %{conn: conn} do
       conn = get(conn, Routes.session_path(conn, :new))
       assert html_response(conn, 200) =~ "Sign in"
     end
 
-    test "renders index when credentials are valid", %{conn: conn} do
+    test "POST /sign-in renders index when credentials are valid", %{conn: conn} do
       Accounts.create_user(@user_attrs)
       conn = post(conn, Routes.session_path(conn, :create), user: @user_attrs)
       assert get_flash(conn, :info) =~ "welcome back!"
