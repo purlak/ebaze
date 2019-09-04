2b587d34ff23767114c0a22b9a35ceb1bfcdb513
Purnima Lakshminarayanan <purnima@8thlight.com>
2019-09-04 10:30:09 -0400

    [LANDING PAGE] display auction listings

--- test/ebaze_web/controllers/page_controller_test.exs
+++ test/ebaze_web/controllers/page_controller_test.exs
@@ -7,5 +7,11 @@ defmodule EbazeWeb.PageControllerTest do
       assert html_response(conn, 200) =~ "Welcome to Ebaze!"
       assert html_response(conn, 200) =~ "New auction items listed everyday. Sell or Bid now!"
     end
+
+    test "GET / displays auction listings", %{conn: conn} do
+      conn = get(conn, "/")
+
+      assert conn.assigns[:auctions]
+    end
   end
 end
