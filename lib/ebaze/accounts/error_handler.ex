c9bd3dc174663fcad45029728bc7820e30a13372
Purnima Lakshminarayanan <purnima@8thlight.com>
2019-09-11 16:05:38 -0400

    [AUTH] refactor unauthenticated handler

--- lib/ebaze/accounts/error_handler.ex
+++ lib/ebaze/accounts/error_handler.ex
@@ -6,16 +6,8 @@ defmodule Ebaze.Accounts.ErrorHandler do
 
   @impl Guardian.Plug.ErrorHandler
   def auth_error(conn, {type, _reason}, _opts) do
-    body = to_string(type)
-
-    if body == "unauthenticated" do
       conn
       |> put_flash(:error, "Sign-in to continue")
       |> redirect(to: Routes.session_path(conn, :new))
-    else
-      conn
-      |> put_resp_content_type("text/plain")
-      |> send_resp(401, body)
-    end
   end
 end
